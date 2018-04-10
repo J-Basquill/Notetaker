5df11ab329acf7a037ccca612159281a23ff4a88
J-Basquill <james.basquill@gmail.com>
2018-04-10 13:28:42 +0100

    Uploader Downloader functioning

--- notetaker/src/components/Downloader.js
+++ notetaker/src/components/Downloader.js
@@ -1,17 +1,44 @@
 import React from "react";
+import * as firebase from "firebase";
 //git check comment
 export default class Downloader extends React.Component{
+    download() {
+        let storageRef =  firebase.storage().ref();
+        let path = document.getElementById("filePath").value;
+        let imgRef = storageRef.child(path);
+        imgRef.getDownloadURL().then(function(url) {
+            console.log("Download check");
+            console.log(url);
+
+
+        }.bind(this)).catch(function(error) {
+            console.log(error);
+        });
+    }
+
+    showList(){
+        let userId = document.getElementById("username").value;
+        return firebase.database().ref('files/' + userId).once('value', function(snapshot) {
+            snapshot.forEach(function(childSnapshot) {
+                childSnapshot.forEach(function(childChildSnapshot){
+                    document.getElementById("list").innerText += childChildSnapshot.key+": "+childChildSnapshot.val()+"\n";
+                });
+            });
+        });
+
+    }
+
     render(){
         return(
             <div>
             <h1>DOWNLOADING</h1>
         Username:<input type="text name=" id="username"/>
-            <button id="lis">List?</button><br/>
+            <button id="lis" onClick={this.showList.bind(this)}>List?</button><br/>
         File Path:<input type="text name=" id="filePath"/>
-            <button id="dl">Download</button><br/>
-            <button id="j2u">Go to Uploading</button><br/>
+            <button id="dl" onClick={this.download.bind(this)}>Download</button><br/>
         <p id="list"></p>
             </div>
     );
     }
 }
+
--- notetaker/src/components/Uploader.js
+++ notetaker/src/components/Uploader.js
@@ -1,21 +1,64 @@
 import React from "react";
+import * as firebase from "firebase";
 //git check comment
+
+
+let username = "immy",
+    uploader = document.getElementById("uploader"),
+    //fileButton = document.getElementById("fileButton"),
+    //user = document.getElementById("username"),
+    demo = document.getElementById("demo"),
+    upper = document.getElementById("up"),
+    listB = document.getElementById("lis");
+
+
+
 export default class Uploader extends React.Component{
+    upload(){
+        var stRef = firebase.storage();
+        var dbRef = firebase.database();
+        var username = document.getElementById("username").value;
+        var file = document.getElementById("fileButton").files[0];
+        var storageRef = firebase.storage().ref(username+"/"+file.name);
+        var task = storageRef.put(file);
+        var path = storageRef.fullPath;
+        task.on("state_changed",
+            function prgress(snapshot){
+                var percentage = (snapshot.bytesTransferred/snapshot.totalBytes)*100;
+                document.getElementById("uploader").value = percentage;
+            },
+            function complete(){
+            });
+        //document.getElementById("demo").innerHTML= path;
+        dbRef.ref("files/"+username+"/").push().set({
+            file: path
+        });
+        // uploadRef(username, path);
+    }
     render(){
+
         return(
             <div>
             <h1>UPLOADING</h1>
         Username:<input type="text name=" id="username"/>
             <input type="file" id="fileButton"/>
-            <button id="up">Submit?</button><br/>
+            <button id="up" onClick={this.upload.bind(this)}>Submit?</button><br/>
         <progress value="0" max="100" id="uploader">0%</progress>
-            <button id="lis">List?</button><br/>
-        File Path:<input type="text name=" id="filePath"/>
-            <button id="dl">Download</button><br/>
-            <button id="j2d">Go to Downloading</button><br/>
-        <p id="list"></p>
 
             </div>
+
     );
+
     }
+
+
+
+
+
+// uploadRef(usern, fileRef){
+//     dbRef.ref("files/"+usern+"/").push().set({
+//         file: fileRef
+//     });
+// }
+
 }
