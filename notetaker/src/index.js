a80ee7ab8cb8eafb41509dfe35123d24b8ca7eba
SatinderK <SatinderK@users.noreply.github.com>
2018-04-28 18:18:34 +0100

    css

--- notetaker/src/index.js
+++ notetaker/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
 import '@blueprintjs/core/dist/blueprint.css';
+import "./App.css";
 import App from './App';
 import{ BrowserRouter } from 'react-router-dom';
 
